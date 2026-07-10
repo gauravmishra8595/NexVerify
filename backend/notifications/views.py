8e75d593fe688a810086a624de8e70e10b1c751d
gauravmishra8595 <gauravmishrag999@gmail.com>
2026-07-10 20:48:57 +0530

    Update smtp test endpoint

--- backend/notifications/views.py
+++ backend/notifications/views.py
@@ -1,14 +1,8 @@
-import socket
+
 from django.http import JsonResponse
 
 def smtp_test(request):
-    try:
-        ip = socket.gethostbyname("smtp.gmail.com")
-        return JsonResponse({
-            "resolved": ip
-        })
-    except Exception as e:
-        return JsonResponse({
-            "error": str(e)
-        }, status=500)
+    return JsonResponse({
+        "version": "SMTP TEST V2"
+    })
 # Create your views here.
