9e7f0a72ed517a3c306663c2f3ae5d30a94652fb
gauravmishra8595 <gauravmishrag999@gmail.com>
2026-07-13 20:05:53 +0530

    Modified settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -181,7 +181,14 @@ AUTHENTICATION_BACKENDS = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
-RESEND_API_KEY = os.getenv("RESEND_API_KEY")
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
